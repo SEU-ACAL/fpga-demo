6211d98475f949ec86c9c902738b9d49cc503a39
shirohasuki <whmio0115@gmail.com>
2026-04-27 22:48:35 +0800

    refactor: update script sourcing in debug.tcl for improved path handling

--- examples/p2e_ddr4_backdoor/debug.tcl
+++ examples/p2e_ddr4_backdoor/debug.tcl
@@ -1,12 +1,13 @@
-source debug_normal.tcl
+set script_dir [file dirname [file normalize [info script]]]
+source [file join $script_dir debug_normal.tcl]
 
 #hw_server -release;design -close
 
-#source debug_normal_16G.tcl
+#source [file join $script_dir debug_normal_16G.tcl]
 
 #hw_server -release;design -close
 
-#source debug_backdoor_0p5G.tcl
+#source [file join $script_dir debug_backdoor_0p5G.tcl]
 
 exit 
 
